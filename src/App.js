68cecb46a31b2df72ccd973ed57aad9b900c487d
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-07-30 23:56:15 +1000

    added the set counter function> basic> onto using the props drilling first

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import "./App.css";
 import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <span>{counter}</span>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
